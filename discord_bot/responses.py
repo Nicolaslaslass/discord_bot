da7c0cc84d45f8b93f109681f8807cf3cde123f2
NVE <nicolas.vergara.e@gmail.com>
2023-07-14 19:36:04 -0400

    bot escrito, listo para primera prueba

---
+++ discord_bot/responses.py
@@ -0,0 +1,9 @@
+def handle_response(message) -> str:
+    p_message = message.lowercase() #en caso de que venga con mayusculas 
+    
+    if p_message == '$toss coin':
+        return 'prueba' #meter def de cointoss
+    
+
+    
+    
